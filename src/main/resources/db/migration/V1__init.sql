579efa6a1ad1f40ced1637a589c6e82d5be165b5
Lucas Guardiola <115181587+ljguardiola@users.noreply.github.com>
2024-04-23 21:25:53 -0300

    Config flyway

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -7,3 +7,12 @@ CREATE TABLE accounts
     initial_balance DOUBLE PRECISION NOT NULL,
     CONSTRAINT pk_accounts PRIMARY KEY (id)
 );
+
+CREATE TABLE users
+(
+    id UUID NOT NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
+
+ALTER TABLE accounts
+    ADD CONSTRAINT FK_ACCOUNTS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
